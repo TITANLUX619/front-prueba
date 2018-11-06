250f6cc87b9fd81a9c3d4f08d86cd07dad54ca82
TITANLUX619 <antlopsou@gmail.com>
2018-11-06 13:47:31 +0100

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios'; 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        resources: []
+    };
+}
+  
+  componentDidMount() {
+    axios.get('https://guadaltech-fulllstack.herokuapp.com/api/becarios/')
+        .then(res => {
+            this.setState({ resources: res.data });
+            console.log(this.state.resources);
+        });
+}
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <div className="container">
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h3 className="panel-title">
+                    Resources List
+                  </h3>
+                  </div>        
+                  <div>
+                    <div className="panel-body">
+                      <table className="table table-hover table-dark">
+                        <thead>
+                          <tr>
+                            <th>DNI</th>
+                              <th>First Name</th>
+                                <th>Last Name</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {this.state.resources.map(resource =>
+                            <tr>
+                              <td><Link to={`/show/${resource.dni}`}>{resource.dni}</Link></td>
+                              <td>{resource.firstName}</td>
+                              <td>{resource.lastName}</td>
+                            </tr>
+                          )}
+                        </tbody>
+                      </table>
+                    </div>      
+                  </div>    
+                </div>
+            </div>
+        </div>
     );
-  }
+}
 }
 
 export default App;
